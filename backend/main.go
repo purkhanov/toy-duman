494dcd1ce63d711e405bfcfe23515aaffaf5a41e
Nurlan Purkhanov <ppurkhanov@gmail.com>
2025-06-18 23:33:05 +0500

    edited main

--- backend/main.go
+++ backend/main.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"toy-duman/database"
+	"toy-duman/web"
+)
+
+func main() {
+	err := database.InitDB()
+	if err != nil {
+		log.Fatalf("Error initializing database: %v", err)
+	}
+
+	server := web.NewServer()
+	err = server.Start()
+	if err != nil {
+		log.Fatalf("Error starting web server: %v", err)
+		return
+	}
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGHUP, syscall.SIGTERM)
+
+	sig := <-sigCh
+	log.Printf("Received signal: %s, stopping web server...", sig)
+
+	err = server.Stop()
+	if err != nil {
+		log.Printf("Error stopping web server: %v", err)
+	}
+}

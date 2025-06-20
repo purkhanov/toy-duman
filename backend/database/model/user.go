2c6f52263dd982dfdccd01ca730965a782e0c5da
Nurlan Purkhanov <ppurkhanov@gmail.com>
2025-06-20 23:19:39 +0500

    updated user model

--- backend/database/model/user.go
+++ backend/database/model/user.go
@@ -1,13 +1,14 @@
 package model
 
 type User struct {
-	ID       int    `json:"id" gorm:"primaryKey;autoIncrement"`
-	Username string `json:"username"`
-	Name     string `json:"name"`
+	ID       int      `json:"id" gorm:"primaryKey;autoIncrement"`
+	Username string   `json:"username"`
+	Name     string   `json:"name"`
+	Status   []Status `json:"status" gorm:"foreignKey:UserID;references:ID;constraint:OnDelete:CASCADE"`
 }
 
 type Status struct {
 	ID     int    `json:"id" gorm:"primaryKey;autoIncrement"`
 	Link   string `json:"link"`
-	UserID int    `json:"user_id" gorm:"foreignKey:User;references:ID"`
+	UserID int    `json:"user_id"`
 }
